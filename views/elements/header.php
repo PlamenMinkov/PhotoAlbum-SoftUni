863a57eefd7c0fdf6db6f308b4c955e5e65943e5
PlamenMinkov <minkov.plamen@yahoo.com>
2015-04-29 11:15:19 +0300

    connect to database

--- views/elements/header.php
+++ views/elements/header.php
@@ -1,6 +1,17 @@
 <?php
     session_start();
     //include 'includes/functions.php';
+    include_once 'libs/Database.php';
+    
+    $db = Database::getInstance();
+    $db->setParameters('localhost', 'root', '', 'photo_album');
+    $connection = $db->getConnection();
+    mysqli_query($connection, 'SET NAMES utf8');
+    mb_internal_encoding('UTF-8');
+    if (!$connection) {
+        echo 'Сриване на системата!!!';
+        exit();
+    }
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
